e93811bd1493580d2a74f5809ea6b5fe1222a1e8
AyoSynk <161512257+synkfr@users.noreply.github.com>
2025-01-28 01:28:45 +0530

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -27,7 +27,6 @@ $cmr = new Customer();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Poppins" rel="stylesheet">
     <script src="js/bootstrap.bundle.min.js"></script>
